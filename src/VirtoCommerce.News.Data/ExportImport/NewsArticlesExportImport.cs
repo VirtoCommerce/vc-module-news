c42374d3140f6e5a6c1fe0c95f4f3f3a2e1eaeb7
yuskithedeveloper <yuskianywhere@gmail.com>
2025-07-28 14:48:56 -0500

    Refactoring

--- src/VirtoCommerce.News.Data/ExportImport/NewsArticlesExportImport.cs
+++ src/VirtoCommerce.News.Data/ExportImport/NewsArticlesExportImport.cs
@@ -17,32 +17,32 @@ public class NewsArticlesExportImport(INewsArticleService newsArticleService, IN
     public async Task DoExportAsync(Stream outStream, Action<ExportImportProgressInfo> progressCallback, ICancellationToken cancellationToken)
     {
         cancellationToken.ThrowIfCancellationRequested();
+
         var progressInfo = new ExportImportProgressInfo { Description = "News articles exporting..." };
         progressCallback(progressInfo);
 
-        using (var sw = new StreamWriter(outStream, System.Text.Encoding.UTF8))
-        using (var writer = new JsonTextWriter(sw))
-        {
-            await writer.WriteStartObjectAsync();
-            await writer.WritePropertyNameAsync("NewsArticles");
+        using var streamWriter = new StreamWriter(outStream, System.Text.Encoding.UTF8);
+        using var writer = new JsonTextWriter(streamWriter);
 
-            await writer.SerializeArrayWithPagingAsync(
-                serializer,
-                _batchSize,
-                async (skip, take) =>
-                {
-                    return (GenericSearchResult<NewsArticle>)await newsArticleSearchService.SearchAsync(new NewsArticleSearchCriteria { Skip = skip, Take = take });
-                },
-                (processedCount, totalCount) =>
-                {
-                    progressInfo.Description = $"{processedCount} of {totalCount} news articles have been exported";
-                    progressCallback(progressInfo);
-                },
-                cancellationToken);
+        await writer.WriteStartObjectAsync();
+        await writer.WritePropertyNameAsync("NewsArticles");
 
-            await writer.WriteEndObjectAsync();
-            await writer.FlushAsync();
-        }
+        await writer.SerializeArrayWithPagingAsync(
+            serializer,
+            _batchSize,
+            async (skip, take) =>
+            {
+                return (GenericSearchResult<NewsArticle>)await newsArticleSearchService.SearchAsync(new NewsArticleSearchCriteria { Skip = skip, Take = take });
+            },
+            (processedCount, totalCount) =>
+            {
+                progressInfo.Description = $"{processedCount} of {totalCount} news articles have been exported";
+                progressCallback(progressInfo);
+            },
+            cancellationToken);
+
+        await writer.WriteEndObjectAsync();
+        await writer.FlushAsync();
     }
 
     public async Task DoImportAsync(Stream inputStream, Action<ExportImportProgressInfo> progressCallback, ICancellationToken cancellationToken)
@@ -52,37 +52,36 @@ public class NewsArticlesExportImport(INewsArticleService newsArticleService, IN
         var progressInfo = new ExportImportProgressInfo { Description = "News articles importing..." };
         progressCallback(progressInfo);
 
-        using (var streamReader = new StreamReader(inputStream))
-        using (var reader = new JsonTextReader(streamReader))
+        var streamReader = new StreamReader(inputStream);
+        var reader = new JsonTextReader(streamReader);
+
+        while (await reader.ReadAsync())
         {
-            while (await reader.ReadAsync())
+            if (reader.TokenType != JsonToken.PropertyName)
             {
-                if (reader.TokenType != JsonToken.PropertyName)
-                {
-                    continue;
-                }
+                continue;
+            }
 
-                if (reader.Value.ToString() == "NewsArticles")
-                {
-                    await reader.DeserializeArrayWithPagingAsync<NewsArticle>(
-                        serializer,
-                        _batchSize,
-                        async items =>
+            if (reader.Value.ToString() == "NewsArticles")
+            {
+                await reader.DeserializeArrayWithPagingAsync<NewsArticle>(
+                    serializer,
+                    _batchSize,
+                    async items =>
+                    {
+                        foreach (var item in items)
                         {
-                            foreach (var item in items)
-                            {
-                                item.SetIsPublished(item.IsPublished);
-                            }
+                            item.SetIsPublished(item.IsPublished);
+                        }
 
-                            await newsArticleService.SaveChangesAsync(items.ToArray());
-                        },
-                        processedCount =>
-                        {
-                            progressInfo.Description = $"{processedCount} news articles have been imported";
-                            progressCallback(progressInfo);
-                        },
-                        cancellationToken);
-                }
+                        await newsArticleService.SaveChangesAsync(items.ToArray());
+                    },
+                    processedCount =>
+                    {
+                        progressInfo.Description = $"{processedCount} news articles have been imported";
+                        progressCallback(progressInfo);
+                    },
+                    cancellationToken);
             }
         }
     }
